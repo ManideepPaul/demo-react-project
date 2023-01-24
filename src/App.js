73ab50ce433993bca4f26aa2837a21a098d7424f
Manideep <17101992m@gmail.com>
2023-01-24 20:50:20 +0530

    Completed lecture 40

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
     this.state = {
       monsters: []
     }
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(user => this.setState(() => {
@@ -23,8 +25,13 @@ class App extends Component {
     }))
   }
   render() {
+    console.log('render')
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(e) => {
+          const filteredMonster = this.state.monsters.filter((monster) => monster.name.includes(e.target.value)) ;
+          this.setState({monsters: filteredMonster})
+        }}/>
         {this.state.monsters.map((monster, index) => <h1 key={index}>{monster.name}</h1>)}
       </div>
     );
